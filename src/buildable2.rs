975d6203e809c60b940c8f9d8645be06b092c74d
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-03-14 15:54:12 -0700

    buildable2

--- src/buildable2.rs
+++ src/buildable2.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 use dyn_clone::DynClone;
 
-use crate::{iso::ItemContainerMap, prelude::*};
+use crate::{
+    iso::{ItemContainerMap, SpatialMap},
+    prelude::*,
+};
 
 pub struct BuildingContext<'a> {
     pub commands: &'a mut Commands,
@@ -16,15 +19,32 @@ pub struct MutBuildingMaps<'a> {
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Map {
+pub enum WhichMap {
     Buildings,
     Conveyors,
     ItemContainers,
 }
 
+impl WhichMap {
+    pub fn get_from_maps_mut<'b>(
+        self,
+        maps: &'b mut MutBuildingMaps,
+    ) -> &'b mut SpatialMap<Entity> {
+        match self {
+            WhichMap::Buildings => &mut **maps.buildings,
+            WhichMap::Conveyors => &mut **maps.conveyors,
+            WhichMap::ItemContainers => &mut **maps.item_containers,
+        }
+    }
+}
+
+pub struct Built {
+    pub buildable: Box<dyn Buildable>,
+}
+
 pub trait Buildable: DynClone + Sync + Send + 'static {
     fn shape(&self, ctx: &mut BuildingContext) -> Vec<IsoPos>;
-    fn maps(&self) -> Vec<Map>;
+    fn maps(&self) -> Vec<WhichMap>;
     fn extra_root_components(&self, ctx: &mut BuildingContext);
     #[allow(unused_variables)]
     fn spawn_extras(&self, ctx: &mut BuildingContext, maps: &mut MutBuildingMaps) -> Vec<Entity> {
@@ -38,33 +58,87 @@ pub fn spawn_buildable(
     ctx: &mut BuildingContext,
     maps: &mut MutBuildingMaps,
 ) -> Entity {
+    let root = spawn_root(&buildable, ctx);
+    BuildableSpawner {
+        buildable: &*buildable,
+        root,
+        ctx,
+        maps,
+    }
+    .finish_spawning()
+}
+
+fn spawn_root(buildable: &Box<dyn Buildable>, ctx: &mut BuildingContext) -> Entity {
     let built = Built {
-        buildable: dyn_clone::clone_box(&*buildable),
+        buildable: dyn_clone::clone_box(&**buildable),
     };
     let root = ctx.commands.spawn((built,)).current_entity().unwrap();
-    for extra in buildable.spawn_extras(ctx, maps) {
-        ctx.commands.set_current_entity(extra);
-        ctx.commands.with(Parent(root));
-    }
-    for art in buildable.spawn_art(ctx) {
-        ctx.commands.set_current_entity(art);
-        ctx.commands.with(Parent(root));
-    }
-    let requested_maps = buildable.maps();
-    for pos in buildable.shape(ctx) {
-        if requested_maps.contains(&Map::Buildings) {
-            maps.buildings.set_assuming_empty(pos, root);
-        }
-        if requested_maps.contains(&Map::Conveyors) {
-            maps.conveyors.set_assuming_empty(pos, root);
-        }
-        if requested_maps.contains(&Map::ItemContainers) {
-            maps.item_containers.set_assuming_empty(pos, root);
-        }
-    }
     root
 }
 
-pub struct Built {
-    pub buildable: Box<dyn Buildable>,
+struct BuildableSpawner<'a, 'b, 'c> {
+    buildable: &'a dyn Buildable,
+    root: Entity,
+    ctx: &'a mut BuildingContext<'b>,
+    maps: &'a mut MutBuildingMaps<'c>,
+}
+
+impl<'a, 'b, 'c> BuildableSpawner<'a, 'b, 'c> {
+    fn finish_spawning(mut self) -> Entity {
+        self.spawn_art();
+        self.spawn_others();
+        self.mark_positions_on_maps();
+        self.root
+    }
+
+    fn mark_positions_on_maps(&mut self) {
+        let requested_maps = self.buildable.maps();
+        for map in requested_maps {
+            let map = map.get_from_maps_mut(self.maps);
+            for pos in self.buildable.shape(self.ctx) {
+                map.set_assuming_empty(pos, self.root);
+            }
+        }
+    }
+
+    fn spawn_art(&mut self) {
+        for art in self.buildable.spawn_art(self.ctx) {
+            self.ctx.commands.set_current_entity(art);
+            self.ctx.commands.with(Parent(self.root));
+        }
+    }
+
+    fn spawn_others(&mut self) {
+        for extra in self.buildable.spawn_extras(self.ctx, self.maps) {
+            self.ctx.commands.set_current_entity(extra);
+            self.ctx.commands.with(Parent(self.root));
+        }
+    }
+}
+
+pub fn destroy_buildable(
+    buildable: (Entity, &Built),
+    ctx: &mut BuildingContext,
+    maps: &mut MutBuildingMaps,
+) {
+    // The spawner parents everything to the root entity, so this will take care
+    // of all art and other related entities as well as the buildable object
+    // itself.
+    ctx.commands.despawn_recursive(buildable.0);
+    let buildable = &buildable.1.buildable;
+    clear_positions_on_maps(buildable, maps, ctx);
+}
+
+fn clear_positions_on_maps(
+    buildable: &Box<dyn Buildable>,
+    maps: &mut MutBuildingMaps,
+    ctx: &mut BuildingContext,
+) {
+    let requested_maps = buildable.maps();
+    for map in requested_maps {
+        let map = map.get_from_maps_mut(maps);
+        for pos in buildable.shape(ctx) {
+            map.clear(pos);
+        }
+    }
 }
